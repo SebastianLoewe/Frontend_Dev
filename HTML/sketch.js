75ea3bdbc1781e2dc162274f26720114fe3632b9
Sebastian Loewe <66331097+SebastianLoewe@users.noreply.github.com>
2021-01-21 13:35:57 +0100

    Update sketch.js

--- HTML/sketch.js
+++ HTML/sketch.js
@@ -8,3 +8,14 @@ function draw() {
   fill(0, 0, mouseX);
   circle(mouseX, mouseY, 30);
 }
+
+function windowResized() {
+  if (windowWidth >= 600) {
+    resizeCanvas(windowWidth * 0.7, windowHeight * 0.7);
+    console.log('w-size: 70%');
+    console.log(windowWidth);
+  } else if (windowWidth < 600) {
+    resizeCanvas(windowWidth*0.95, windowHeight*0.6);
+    console.log('w-size: full');
+  }
+}
